3de0509254a4d41d4eb1fd9c59eab00d242bee02
oyerindedaniel <oyerinde.daniel@yahoo.com>
2024-01-05 17:14:19 +0100

    fixes

--- next.config.js
+++ next.config.js
@@ -6,13 +6,19 @@ await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
-  //   webpack(config) {
-  //     config.module.rules.push({
-  //       test: /\.svg$/,
-  //       use: ['@svgr/webpack']
-  //     });
-  //     return config;
-  //   }
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ['@svgr/webpack']
+  //   });
+
+  //   return config;
+  // },
+  reactStrictMode: true
 };
 
 export default config;
--- src/app/platform-launch/page.tsx
+++ src/app/platform-launch/page.tsx
@@ -51,4 +51,3 @@ const PlatformLaunch = () => {
 };
 
 export default PlatformLaunch;
-
