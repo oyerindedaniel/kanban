77e79be0603046165daed625ca0d275f62d48260
oyerindedaniel <oyerinde.daniel@yahoo.com>
2024-07-07 16:22:26 +0100

    fixing build

--- src/Providers/index.tsx
+++ src/Providers/index.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { ReduxProviders } from '@/store/provider';
+// import { ReduxProviders } from '@/store/provider';
 import { ThemeProvider } from './theme-provider';
 import { MockProvider } from './mock-provider';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     // <MockProvider>
-    <ReduxProviders>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-        {children}
-      </ThemeProvider>
-    </ReduxProviders>
+    // <ReduxProviders>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      {children}
+    </ThemeProvider>
+    // </ReduxProviders>
     // </MockProvider>
   );
 }
