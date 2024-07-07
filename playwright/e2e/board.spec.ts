993c2e6e8839f40d0f937a971dd38c94ac80fcef
oyerindedaniel <oyerinde.daniel@yahoo.com>
2024-07-07 01:50:27 +0100

    fixes

--- playwright/e2e/board.spec.ts
+++ playwright/e2e/board.spec.ts
@@ -1,5 +1,3 @@
-// import test from '../fixture';
-
 import test, { expect } from '@playwright/test';
 
 test.describe('board action', () => {
@@ -7,23 +5,8 @@ test.describe('board action', () => {
     await page.goto('/');
   });
 
-  // test('given an empty board array: display create a new board to get started', async ({
-  //   page,
-  //   port,
-  //   http,
-  //   requestInterceptor
-  // }) => {
-  //   requestInterceptor.use(
-  //     http.get(`http://localhost:3000/*`, () => HttpResponse.json({ data: { data: [] } }))
-  //   );
-  //   await page.goto(`http://localhost:${port}/`);
-  //   // It renders a button to add new board.
-  //   await expect(page.getByRole('button', { name: '+ Add New Board' })).toBeVisible();
-  // });
-
   test('given an empty board Column array: shows the correct error messages', async ({ page }) => {
     await page.route('http://localhost:3000/api/trpc/board.create?batch=1', (route) => {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
       route.fulfill({
         status: 200,
         headers: { 'Content-Type': 'application/json' },
@@ -42,10 +25,8 @@ test.describe('board action', () => {
     });
 
     await page.getByRole('button', { name: 'add new board + Create New' }).click();
-
     await page.getByLabel(/Board Name/i).fill('walk cat');
     await page.getByTestId('button.0').click();
-
     await page.getByRole('button', { name: /Create New Board/i }).click();
 
     await expect(page.getByRole('alert')).toBeVisible();
