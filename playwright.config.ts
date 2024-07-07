9213cbc47501363e79487b5002e8399d974fa0a9
oyerindedaniel <oyerinde.daniel@yahoo.com>
2024-07-07 02:28:57 +0100

    added webserver running before strating the tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,12 +68,13 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ]
+  ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm build && pnpm run start',
+    url: process.env.BASE_URL ?? 'http://localhost:3000',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI
+  }
 });
